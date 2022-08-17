b60bb52d8776b1c228bf9c8197756f00f2e02476
Bria <brialinza@gmail.com>
2022-08-16 23:36:43 -0600

    implementing postgresql

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const db_url = process.env.DATABASE_URL;
 
-express()
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/api/', (req, res) => {
+  console.log(db_url);
+  return res.send("Recieved a GET HTTP method with database" + db_url);
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
